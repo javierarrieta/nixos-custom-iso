58a42629b350d9df0d9e3cb306020c372b8830dc
Javier Arrieta <javierarrieta@users.noreply.github.com>
2026-02-19 16:10:10 +0100

    Allow root login

--- iso-config.nix
+++ iso-config.nix
@@ -8,10 +8,16 @@
   networking.networkmanager.enable = true;
   networking.wireless.enable = lib.mkForce false;
 
+  users.users.root.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAxtDTZvN/YqOQC1nOGahb/qLp35iYnBTPaGld6/N6k root@host" 
+  ];
+
   # Optional: Embed your SSH public key for remote access
   users.users.nixos.openssh.authorizedKeys.keys = [
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIJAxtDTZvN/YqOQC1nOGahb/qLp35iYnBTPaGld6/N6k user@host"
   ];
+
+  services.openssh.settings.PermitRootLogin = "yes";
 
   # Allow unfree software (like drivers)
   nixpkgs.config.allowUnfree = true;
